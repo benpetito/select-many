800a6e6247850769dda3bffdbeb03cc5af93820c
Ben Petito <benpetito@gmail.com>
2023-07-18 16:24:45 +0930

    Implement add event and move event in schedule demo
    - update ScheduleView with backing bean methods to support client interactions
    - update AppointmentService with service methods to update/create events

--- src/main/java/au/com/bizhub/faces/ScheduleView.java
+++ src/main/java/au/com/bizhub/faces/ScheduleView.java
@@ -9,11 +9,15 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.schedule.ScheduleEntryMoveEvent;
 import org.primefaces.model.DefaultScheduleEvent;
 import org.primefaces.model.LazyScheduleModel;
+import org.primefaces.model.ScheduleEvent;
 import org.primefaces.model.ScheduleModel;
 import org.skyve.impl.web.faces.beans.FacesView;
 import org.skyve.util.Time;
+import org.skyve.util.Util;
 
 import modules.selectMany.Appointment.AppointmentExtension;
 import modules.selectMany.Appointment.AppointmentService;
@@ -24,8 +28,9 @@ public class ScheduleView extends FacesView<modules.selectMany.domain.ScheduleVi
 
 	private static final long serialVersionUID = 2303644269039081613L;
 
-	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm-HH:mm");
+	private ScheduleEvent<?> event = new DefaultScheduleEvent<>();
 	private ScheduleModel eventModel;
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm-HH:mm");
 
 	@Inject
 	private transient AppointmentService appointmentService;
@@ -51,13 +56,72 @@ public class ScheduleView extends FacesView<modules.selectMany.domain.ScheduleVi
 							.title(appt.getLocation())
 							.startDate(startDateTime)
 							.endDate(endDateTime)
+							.data(appt.getBizId()) // the bizId of the Skyve record
 							.build());
 				}
 			}
 		};
 	}
 
+	public void addEvent() {
+		Util.LOGGER.info("Add new event");
+		if (event.isAllDay()) {
+			// see https://github.com/primefaces/primefaces/issues/1164
+			if (event.getStartDate().toLocalDate().equals(event.getEndDate().toLocalDate())) {
+				event.setEndDate(event.getEndDate().plusDays(1));
+			}
+		}
+
+		if (event.getId() == null) {
+
+			// create a corresponding appointment in the Skyve database when an event is added to the underlying model
+			AppointmentExtension newAppt = appointmentService.createAppointment(event.getStartDate(), event.getEndDate(),
+					event.getTitle());
+			DefaultScheduleEvent newEvent = (DefaultScheduleEvent) event;
+			newEvent.setData(newAppt.getBizId());
+
+			eventModel.addEvent(newEvent);
+		} else {
+			eventModel.updateEvent(event);
+		}
+
+		// reset the selected event to a new one for the next interaction
+		event = new DefaultScheduleEvent<>();
+	}
+
+	public ScheduleEvent<?> getEvent() {
+		return event;
+	}
+
 	public ScheduleModel getEventModel() {
 		return eventModel;
+	}
+
+	public void onDateSelect(SelectEvent<LocalDateTime> selectEvent) {
+		event = DefaultScheduleEvent.builder()
+				.startDate(selectEvent.getObject())
+				.endDate(selectEvent.getObject().plusHours(1))
+				.build();
+	}
+
+	/**
+	 * Update the Skyve {@link AppointmentExtension} when the appointment
+	 * is moved on the schedule.
+	 */
+	public void onEventMove(ScheduleEntryMoveEvent moveEvent) {
+		Util.LOGGER.info(String.format("Event %s moved by days %s %s", moveEvent.getScheduleEvent().getId(),
+				Integer.valueOf(moveEvent.getDayDelta()),
+				moveEvent.getScheduleEvent().getStartDate()));
+
+		final String apptBizId = (String) moveEvent.getScheduleEvent().getData();
+		appointmentService.updateAppointmentDate(apptBizId, moveEvent.getDayDelta());
+	}
+
+	public void onEventSelect(SelectEvent<ScheduleEvent<?>> selectEvent) {
+		event = selectEvent.getObject();
+	}
+
+	public void setEvent(ScheduleEvent<?> event) {
+		this.event = event;
 	}
 }
--- src/main/java/modules/selectMany/Appointment/AppointmentService.java
+++ src/main/java/modules/selectMany/Appointment/AppointmentService.java
@@ -1,13 +1,21 @@
 package modules.selectMany.Appointment;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 
+import org.skyve.CORE;
+import org.skyve.domain.types.DateOnly;
 import org.skyve.persistence.Persistence;
+import org.skyve.util.Time;
 
 import modules.selectMany.domain.Appointment;
+import modules.selectMany.domain.Appointment.Status;
 
 /**
  * This class acts as a service layer to encapsulate domain logic.
@@ -21,9 +29,52 @@ public class AppointmentService {
 	private transient Persistence persistence;
 
 	/**
+	 * Creates a new appointment with the specified details.
+	 * 
+	 * @param title
+	 */
+	@SuppressWarnings("static-method")
+	public AppointmentExtension createAppointment(LocalDateTime startDate, LocalDateTime endDate, String title) {
+		AppointmentExtension appt = Appointment.newInstance();
+
+		final DateOnly apptDate = Time.asDateOnly(startDate.toLocalDate());
+		appt.setAppointmentDate(apptDate);
+		appt.setLocation(title);
+
+		// hardcoded required fields for example, these should be captured in the schedule dialog
+		appt.setAppointmentTimeValues(Arrays.asList("09:00-09:15", "09:15-09:30"));
+		appt.setStatus(Status.booked);
+
+		return CORE.getPersistence().save(appt);
+	}
+
+	/**
+	 * Retrieve the appointment with the specified identifier.
+	 */
+	@SuppressWarnings("static-method")
+	public AppointmentExtension getAppointment(final String apptBizId) {
+		return CORE.getPersistence().retrieve(Appointment.MODULE_NAME, Appointment.DOCUMENT_NAME, apptBizId);
+	}
+
+	/**
 	 * Returns all appointments.
 	 */
 	public List<AppointmentExtension> getAllAppointments() {
 		return persistence.newDocumentQuery(Appointment.MODULE_NAME, Appointment.DOCUMENT_NAME).beanResults();
+	}
+
+	/**
+	 * Updates the appointment date by the number of days specified.
+	 * @param apptBizId The bizId of the appointment to update
+	 * @param dayDelta The number of days to update
+	 */
+	public void updateAppointmentDate(String apptBizId, int dayDelta) {
+		final AppointmentExtension appointment = getAppointment(apptBizId);
+
+		final DateOnly existingApptDate = appointment.getAppointmentDate();
+		final DateOnly newApptDate = new DateOnly(Date.from(existingApptDate.toInstant().plus(dayDelta, ChronoUnit.DAYS)));
+
+		appointment.setAppointmentDate(newApptDate);
+		CORE.getPersistence().save(appointment);
 	}
 }
